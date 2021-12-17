5e2afe330c3f275f5d97760fa75f0e6616978925
Sandya Dasari <96039095+Sandya-Joyce@users.noreply.github.com>
2021-12-17 16:43:15 +0530

    Testing Datatypes

---
+++ Learning-Dotnet/DisplayVariables/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DisplayVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //combination of both text and variable
+            string name = "John";
+            Console.WriteLine(name);
+
+            //also use + character to add a varaible to another variable
+            string firstName = "John";
+            string lastName = "Doe";
+            string fullName= firstName + lastName;
+            Console.WriteLine(fullName);
+
+            //Numeric values + works as mathematical operation (for integers)
+
+            int x = 5;
+            int y = 6;
+            Console.WriteLine(x+y);
+
+            // declare many variables
+            int X = 5, Y = 6 , Z = 50;
+            Console.WriteLine(X+Y+Z);
+
+
+        }
+
+
+    }
+}
