ec59f2c43ff1af22b13706a4af399d2a5e51bec8
Sandya Dasari <96039095+Sandya-Joyce@users.noreply.github.com>
2021-12-20 11:32:16 +0530

    Type Conversion Checkins

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+       static void Main(String[] args)
+        {
+            var number = "1234";
+            var i = Convert.ToInt32(number);
+            Console.WriteLine(i);
+        }
+
+    }
+}
