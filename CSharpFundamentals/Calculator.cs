628b54de3806e768537328bd6d62d549eda7d167
Sandya Dasari <96039095+Sandya-Joyce@users.noreply.github.com>
2021-12-20 17:30:10 +0530

    CSharpFundamentals

---
+++ CSharpFundamentals/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CSharpFundamentals.Math;
+
+namespace CSharpFundamentals
+
+{
+    public class Calculator
+    {
+        public int add(int a , int b)
+        {
+            return a + b;   
+
+        }
+      
+
+    }
+}
---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpFundamentals.Math
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.add(1, 2);
+            Console.WriteLine(result);
+
+        }
+    }
+}
