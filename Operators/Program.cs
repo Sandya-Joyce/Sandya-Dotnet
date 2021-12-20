a403bb647ff9975c234f1f1acf5212888d6a74da
Sandya Dasari <96039095+Sandya-Joyce@users.noreply.github.com>
2021-12-20 12:10:36 +0530

    Operators - Checkins

---
+++ Operators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            var a = 1;
+            var b = 2;  
+            var c = 3;
+            //Arithematic Operators
+            Console.WriteLine(a + b + c);
+            Console.WriteLine(a + b * c);
+            Console.WriteLine(!(a!=b));
+            Console.WriteLine(a == b);
+
+            //Logical Operators
+            Console.WriteLine(c>b && c>a); // T and T = T
+            Console.WriteLine(c>b && c==a); // T and F = F
+            Console.WriteLine(c > b || c == a); // T or F = T
+
+
+
+        }
+    }
+
+}
