ef2413088d13f1948716d9c94f6faf1d7bb31c2b
Sandya Dasari <96039095+Sandya-Joyce@users.noreply.github.com>
2021-12-22 11:25:16 +0530

    Example-Enum

---
+++ EnumExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EnumExample
+{
+
+    public enum ShippingMethod
+    {
+        RegularAirMail= 1,
+        RegisteredAirMail = 2,
+        Express = 3
+
+    }
+     class Program
+    {
+       static void Main(String[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);   
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingmethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+
+
+        }
+    }
+}
