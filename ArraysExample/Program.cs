840199c998d13987d5fe60cfe8f03d366dfd2b12
Sandya Dasari <96039095+Sandya-Joyce@users.noreply.github.com>
2021-12-22 11:14:02 +0530

    ArrayExample

---
+++ ArraysExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrayExample
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 1; 
+
+            Console.WriteLine(numbers[0]); // intialized to 1 ,i.e numbers[0]=1
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];    
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[3]);
+
+            var names = new string[3] { "John","James", "Mary" };
+
+
+
+
+        }
+    }
+}
