1400393d1bc47b1b2efff5a6973fce1cdadf8b80
Sandya Dasari <96039095+Sandya-Joyce@users.noreply.github.com>
2021-12-29 18:14:18 +0530

    Constructor Example Checkins

---
+++ ConstructorExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyConstructorExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1,"Sandya");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
