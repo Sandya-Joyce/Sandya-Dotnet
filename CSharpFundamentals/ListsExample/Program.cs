40906b2914f1372a96417d78f348c8e9980687e3
Sandya Dasari <96039095+Sandya-Joyce@users.noreply.github.com>
2021-12-30 11:33:44 +0530

    Lists Example

---
+++ CSharpFundamentals/ListsExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Lists
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() {1,2,3, 4,5};
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7 });
+
+            foreach(var number in numbers)
+                Console.WriteLine(number);
+        }
+    }
+}
+
