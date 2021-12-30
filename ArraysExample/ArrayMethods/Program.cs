b324f2fc6bd26e8c012ed8cca170b18a4e62f6c4
Sandya Dasari <96039095+Sandya-Joyce@users.noreply.github.com>
2021-12-30 20:13:26 +0530

    Array Methods

--- ArraysExample/ArrayMethods/Program.cs
+++ ArraysExample/ArrayMethods/Program.cs
@@ -31,11 +31,16 @@ namespace ArrayMethods
                 Console.WriteLine(n);
 
             //Sort
-            Array.Sort(another);
+            Array.Sort(numbers);
             Console.WriteLine("Effect of Sort()");
             foreach (var n in numbers)
                 Console.WriteLine(n);
-
+            
+            //Reverse
+            Array.Reverse(numbers);
+            Console.WriteLine("Effect of Reverse()");
+            foreach (var n in numbers) 
+                Console.WriteLine(n);
 
 
         }
