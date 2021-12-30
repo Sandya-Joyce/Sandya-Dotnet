509fdf6a6b75ff3438cfb32fa857e83ec48b6fda
Sandya Dasari <96039095+Sandya-Joyce@users.noreply.github.com>
2021-12-30 20:09:03 +0530

    Array Methods Checkin

---
+++ ArraysExample/ArrayMethods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ArrayMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, };
+
+            //Length
+            Console.WriteLine("Lenth:"+ numbers.Length);
+
+            //IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9:"+index);
+
+            //Clear
+            Array.Clear(numbers, 0, 2);
+
+            Console.WriteLine("Effect of Clear()");
+            foreach (var n in numbers) 
+            Console.WriteLine(n);
+
+            //Copy
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+            Console.WriteLine("Effect of Copy()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            //Sort
+            Array.Sort(another);
+            Console.WriteLine("Effect of Sort()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+
+
+        }
+    }
+}
